141074ffbdb5974abecd11e0f49a270daf8ad468
Maxim <ranevsky91@mail.ru>
2022-07-26 22:09:02 +0300

    Add id in 'ProductTypeCountModel'

--- Shop/Models/Product/ProductTypeCountModel.cs
+++ Shop/Models/Product/ProductTypeCountModel.cs
@@ -2,7 +2,7 @@
 
 public sealed class ProductTypeCountModel
 {
-    //public int Id { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; } = null!;
     public int Count { get; set; }
 }
